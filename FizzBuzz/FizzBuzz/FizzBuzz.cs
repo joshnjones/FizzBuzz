824f4f47a30b70eca77dc3177dfd0f7b9eec7782
joshnjones <josh@realjoshjones.com>
2024-06-05 09:06:46 -0500

    Add files via upload

---
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,52 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzzProgram
+    {
+        public static void Main(string[] args)
+        {
+            string input = null;
+
+            while (input != "x")
+            {
+                Console.Write("Enter number, or 'x' to quit: ");
+                input = Console.ReadLine();
+
+                if(input != "x")
+                {
+
+                    Console.WriteLine(FizzBuzzCalc(input));
+                }
+            }
+        }
+
+        public static string FizzBuzzCalc(string input)
+        {
+            string result = "";
+            // If user did not enter a number, "Invalid Item"
+            if(string.IsNullOrWhiteSpace(input) || !int.TryParse(input, out int number))
+            {
+                return "Invalid Item";
+            }
+            // Number not divisible by 3 or 5
+            else if (number % 3 != 0 && number % 5 != 0)
+            {
+                return "Divided " + input + " by 3" + System.Environment.NewLine + "Divided " + input + " by 5";
+            } 
+            // Number divisible by either 3 or 5 or both
+            else
+            {
+                if(number % 3 == 0)
+                {
+                    result = "Fizz";
+                }
+                if(number % 5 == 0)
+                {
+                    result += "Buzz";
+                }
+            }
+            return result;
+        }
+    }
+}
+
+
